4aa7a3c46e787c015b090ec49cb18353e791ca14
chaofan85 <fanchao05@gmail.com>
2019-01-21 22:52:13 -0500

    updated App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Visualizer from "./components/visualizer";
+import Player from "./components/player";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Visualizer />
+        <Player />
       </div>
     );
   }
