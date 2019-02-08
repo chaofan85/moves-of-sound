7f0b53a1b1302e6773087c89648fa5a3374a54b8
chaofan85 <fanchao05@gmail.com>
2019-02-07 23:30:06 -0500

    added music

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 
-class Visualizer extends Component {
+class Player extends Component {
   render() {
     return <div />;
   }
 }
+
+export default Player;
